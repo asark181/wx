22a57d8006d8c16be7a62245a6ac346999cfd200
asark181 <111724337+asark181@users.noreply.github.com>
2022-08-22 15:58:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
+ymq_date = os.environ['YMQ_DATE']
 city = os.environ['CITY']
 birthday1 = os.environ['BIRTHDAY1']
 birthday2 = os.environ['BIRTHDAY2']
@@ -25,11 +26,32 @@ def get_weather():
     weather = res['data']['list'][0]
     return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
 
+def get_day():
+    week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    dayOfWeek = datetime.today().weekday()
+    xq = week_list[dayOfWeek]
+    yy = datetime.now().year
+    mm = datetime.now().month
+    dd = datetime.now().day
+    return xq, yy, mm, dd
+
 
 def get_count():
-    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-    return delta.days
+    #delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    #return delta.days
+    next = datetime.strptime(str(date.today().year) + "-" + start_date, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
 
+def get_ymqcount():
+    next = datetime.strptime(str(date.today().year) + "-" + ymq_date, "%Y-%m-%d")
+    if next < datetime.now():
+        les = (today-next).days
+    if les < 7:
+        return les
+    else:
+        return (next.replace(month=next.month+1)-today).days
 
 def get_birthday1():
     next = datetime.strptime(str(date.today().year) + "-" + birthday1, "%Y-%m-%d")
@@ -57,12 +79,19 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
+xq, yy, mm, dd = get_day()
 wea, temperature, minTemperature, maxTemperature = get_weather()
-data = {"weather": {"value": wea},
+data = {"yy": {"value": yy},
+        "mm": {"value": mm},
+        "dd": {"value": dd},
+        "xq": {"value": xq},
+        "city": {"value": city},
+        "weather": {"value": wea},
         "temperature": {"value": temperature},
         "minTemperature": {"value": minTemperature},
         "maxTemperature": {"value": maxTemperature},
         "love_days": {"value": get_count()},
+        "ymq": {"value": get_count()},
         "birthday_left1": {"value": get_birthday1()},
         "birthday_left2": {"value": get_birthday2()},
         "words": {"value": get_words(), "color": get_random_color()}}
